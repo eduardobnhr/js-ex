3a04e26ab3a0cdbca23c566fcbcfdfca9bfc635f
eduardo <eduardobnhr@gmail.com>
2023-08-22 07:31:34 -0400

    add ex

--- variaveis/ex.1.js
+++ variaveis/ex.1.js
@@ -1,11 +1,12 @@
 //declare uma variavel de nome weight
-const weight = 1.78
+//const weight = 1.78
 //variavel do tipo double
 
 
-const person = {
+let student = {
     name: "eduardo",
     age: 22,
-    stars: 1.78,
+    weight: 88.5,
     isSubscribed: true
 }
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg`)
